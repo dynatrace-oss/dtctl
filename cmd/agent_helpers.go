83ec4470097b1eabb75c94d3a26b6f8ac708a9da
MaciejGrynda <85988949+MaciejGrynda@users.noreply.github.com>
2026-03-13 10:22:47 +0100

    some more fixes

--- cmd/agent_helpers.go
+++ cmd/agent_helpers.go
@@ -40,6 +40,10 @@ func extractApplyBase(result apply.ApplyResult) *apply.ApplyResultBase {
 		return &r.ApplyResultBase
 	case *apply.MonitoringConfigApplyResult:
 		return &r.ApplyResultBase
+	case *apply.ExtensionConfigApplyResult:
+		return &r.ApplyResultBase
+	case apply.ExtensionConfigApplyResult:
+		return &r.ApplyResultBase
 	default:
 		return nil
 	}
